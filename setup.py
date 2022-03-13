01f0656f838465520a77f4dbf79152a22ed914e6
amudhan <amudhansubbiah@gmail.com>
2022-03-13 10:15:53 +0530

    Project folder structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='track2',
+    author='amudhan',
+    license='MIT',
+)
